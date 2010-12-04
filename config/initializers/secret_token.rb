62a3cd825c9a541ff671594159d7a0404adffc98
Dennis Bell <djbell@rubidiumstudios.com>
2010-12-03 17:02:33 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shunter::Application.config.secret_token = '0ec6101852a2d53b9409fdb3df01d933f6c7a5aa916e032006be9cc12cbabd1e0d596cc1d9e758d0ab517535dc56c01ec5ac580cc05f6eb9e05c85b3c2d95e86'
